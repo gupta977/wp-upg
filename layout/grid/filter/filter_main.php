6f86fb67bd9fcb0bb30f2079e283b912445086b7
ODude Navneet Gupta <navneet@odude.com>
2019-09-21 14:14:43 +0545

    filter_layout

--- layout/grid/filter/filter_main.php
+++ layout/grid/filter/filter_main.php
@@ -1,30 +1,40 @@
-
-	<tr>
-		<td>
+<tr>
+	<td>
 
 		<?php
-			if ($post_status == "draft")
-				$permalink = 0;
+		if ($post_status == "draft")
+			$permalink = 0;
 
 
-			if ($permalink == "0") {
-				//echo '<img src="'.$image.'" class="pure-img">';
-				echo '<div class="upg_image_container"><img src="' . $image . '"> <div class="upg_image_centered">';
-				echo '<div class="upg_tooltip"><i class="fas fa-eye-slash fa-2x fa-border"></i><span class="upg_tooltiptext">' . __("Under review", "wp-upg") . '</span></div>';
-				echo '</div></div>';
+		if ($permalink == "0") {
+			//echo '<img src="'.$image.'" class="pure-img">';
+			echo '<div class="upg_image_container"><img src="' . $image . '"> <div class="upg_image_centered">';
+			echo '<div class="upg_tooltip"><i class="fas fa-eye-slash fa-2x fa-border"></i><span class="upg_tooltiptext">' . __("Under review", "wp-upg") . '</span></div>';
+			echo '</div></div>';
+		} else {
+			if ($popup == "on") {
+
+				echo '<a data-fancybox="' . $preview_type . '" ' . $extra_param . ' href="' . $preview_large . '" data-caption="' . $thetitle . '" title="' . $thetitle . '" border=0><img src="' . $image . '" style="margin:auto;"></a>';
 			} else {
-				if ($popup == "on") {
-
-					echo '<a data-fancybox="' . $preview_type . '" ' . $extra_param . ' href="' . $preview_large . '" data-caption="' . $thetitle . '" title="' . $thetitle . '" border=0><img src="' . $image . '" style="margin:auto;"></a>';
-				} else {
-					echo '<a href="' . $permalink . '" border="0"><img src="' . $image . '" style="margin:auto;"></a>';
-				}
+				echo '<a href="' . $permalink . '" border="0"><img src="' . $image . '" style="margin:auto;"></a>';
 			}
+		}
 
-			?>
+		?>
 
-		</td>
-		<td><?php echo $thetitle; ?></td>
-		<td><?php echo get_the_date(); ?></td>
-		<td><?php echo upg_show_icon_grid(); ?></td>
-	</tr>	
+	</td>
+	<td><?php echo $thetitle; ?></td>
+	<?php
+
+	for ($x = 1; $x <= 5; $x++) {
+		if ($options['upg_custom_field_' . $x . '_show_front'] == 'on') {
+
+			echo "<td>" . upg_get_value('upg_custom_field_' . $x) . "</td>";
+		}
+	}
+
+	?>
+
+	<td><?php echo get_the_date(); ?></td>
+	<td><?php echo upg_show_icon_grid(); ?></td>
+</tr>
--- layout/grid/filter/filter_up.php
+++ layout/grid/filter/filter_up.php
@@ -1,7 +1,7 @@
     <link href="<?php echo plugins_url() . '/' . upg_FOLDER . '/css/datatables.min.css'; ?>" rel="stylesheet" type="text/css">
     <script src="<?php echo plugins_url() . '/' . upg_FOLDER . '/js/datatables.min.js'; ?>"></script>
-	<script>
-		jQuery(document).ready(function() {
+    <script>
+        jQuery(document).ready(function() {
             jQuery('#table_id').DataTable({
                 "columnDefs": [{
                     "targets": 'no-sort',
@@ -9,14 +9,26 @@
                 }],
             });
         });
-	</script>
-	<table id="table_id" class="display">
+    </script>
+    <table id="table_id" class="display">
         <thead>
             <tr>
                 <th class="no-sort" width="150px">Picture</th>
                 <th>Title</th>
+                <?php
+                for ($x = 1; $x <= 5; $x++) {
+                    if ($options['upg_custom_field_' . $x . '_show_front'] == 'on') {
+
+                        ?>
+                        <th><?php echo upg_get_filed_label('upg_custom_field_' . $x); ?></th>
+
+                <?php
+                    }
+                }
+                ?>
                 <th>Date</th>
+
                 <th class="no-sort">Action</th>
             </tr>
-		</thead>
-		<tbody>
+        </thead>
+        <tbody>
